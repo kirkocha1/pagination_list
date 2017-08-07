3bec934fe7d6e027ec86621d5962a3ff972db74c
kochnev@sibext.com <kochnev@sibext.com>
2017-08-07 11:01:14 +0700

    change fragment initialization workflow

--- app/src/main/java/com/task/paginationlist/presentation/piclist/views/activities/MainActivity.java
+++ app/src/main/java/com/task/paginationlist/presentation/piclist/views/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.task.paginationlist.presentation.piclist.views.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.task.paginationlist.R;
 import com.task.paginationlist.presentation.piclist.views.fragments.FullSizePicFragment;
+import com.task.paginationlist.presentation.piclist.views.fragments.PicGridListFragment;
 import com.task.paginationlist.presentation.piclist.views.utils.UtilsHelper;
 
 public class MainActivity extends MvpAppCompatActivity {
@@ -13,14 +15,17 @@ public class MainActivity extends MvpAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null && UtilsHelper.isTablet(this)) {
-            createFullSizePicFragment();
+        if (savedInstanceState == null) {
+            inflateWithFragments();
         }
     }
 
-    private void createFullSizePicFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.full_part_container, new FullSizePicFragment(), getString(R.string.full_size_fragment_tag))
-                .commit();
+    private void inflateWithFragments() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.list_pic_container, new PicGridListFragment(), getString(R.string.grid_fragment_tag));
+        if (UtilsHelper.isTablet(this)) {
+            transaction.replace(R.id.full_part_container, new FullSizePicFragment(), getString(R.string.full_size_fragment_tag));
+        }
+        transaction.commit();
     }
 }
